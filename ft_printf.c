41d860b74ff63e8ab5e44cf17b6df5ded9ba189d
Kurumi Yamagishi <kuyamagi@student.42tokyo.jp>
2024-12-14 18:49:09 +0900

    2nd

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kuyamagi < kuyamagi@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 14:58:47 by kuyamagi          #+#    #+#             */
-/*   Updated: 2024/12/07 15:57:03 by kuyamagi         ###   ########.fr       */
+/*   Updated: 2024/12/14 16:56:52 by kuyamagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,25 @@ int	ft_printf(const char *format, ...)
 		if(format[i] ==  '%' && format[i+1]){
 			i++;
 			if(format[i] == 'c')
-				ft_c.c//1つの文字を出力
+				ft_char//1つの文字を出力
 			else if(format[i] == 's')
-				ft_s.c//文字列を出力（一般的なCの規約に従う）。
+				ft_char//文字列を出力（一般的なCの規約に従う）。
 			else if(format[i] == 'p')
-				ft_p.c//void * 型のポインタ引数を16進数形式で出力
+				ft_p//void * 型のポインタ引数を16進数形式で出力
 			else if(format[i] == 'd')
-				ft_d.c//10進数の数値を出力
+				ft_d(va_arg(*ptr, int))//10進数の数値を出力
 			else if(format[i] == 'i')
-				ft_i.c//10進数の整数を出力
+				ft_i(va_arg(*ptr, int))//10進数の整数を出力
 			else if(format[i] == 'u')
-				ft_u.c//符号なし10進数の数値を出力
+				ft_u(va_arg(*ptr, unsigned int));//符号なし10進数の数値を出力
 			else if(format[i] == 'x')
-				ft_x.c//数値を16進数の小文字形式で出力
+				ft_x(va_arg(*ptr, unsigned int));//数値を16進数の小文字形式で出力
 			else if(format[i] == 'X')
-				ft_X.c//数値を16進数の大文字で出力
+				ft_X(va_arg(*ptr, unsigned int));//数値を16進数の大文字で出力
 			else if(format[i] == '%')
-				ft_percent.c()//%を返す
+				ft_char('%')//%を返す
+			else
+		len += ft_printf_char(form[i]);
 		}
 	}
 	va_end(args);
--- ft_printf.h
+++ ft_printf.h
@@ -6,8 +6,20 @@
 /*   By: kuyamagi < kuyamagi@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:02:11 by kuyamagi          #+#    #+#             */
-/*   Updated: 2024/12/07 15:50:37 by kuyamagi         ###   ########.fr       */
+/*   Updated: 2024/12/14 16:24:19 by kuyamagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdint.h>
+# include <string.h>
+
+int	ft_printf(const char *format, ...)
+//いっぱい貼り付けるよ〜
+
+#endif
