649072d97089d5f6ecce7e91d225cf8dbda957ed
Kurumi Yamagishi <kuyamagi@student.42tokyo.jp>
2025-04-11 21:10:02 +0900

    あともうちょっと！！！

--- ft_d_i.c
+++ ft_d_i.c
@@ -6,7 +6,7 @@
 /*   By: kuyamagi < kuyamagi@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:17:00 by kuyamagi          #+#    #+#             */
-/*   Updated: 2025/04/11 21:00:50 by kuyamagi         ###   ########.fr       */
+/*   Updated: 2025/04/11 21:06:54 by kuyamagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,33 @@
 int	ft_printf_d_i(int n)
 {
 	int	word_len;
-	int	modorichi;
+	int	ret;
 
 	word_len = 0;
 	if (n == -2147483648)
 	{
-		write(1, "-2147483648", 11);
+		if (write(1, "-2147483648", 11) == -1)
+			return (-1);
 		return (11);
 	}
 	if (n < 0)
 	{
-		modorichi = ft_putchar('-');
-		if(modorichi = -1){
-			return (-1)
-		}
+		ret = ft_putchar('-');
+		if (ret == -1)
+			return (-1);
 		word_len++;
 		n = -n;
 	}
 	if (n >= 10)
-		word_len += ft_printf_d_i(n / 10);
-	ft_putchar((n % 10) + '0');
+	{
+		ret = ft_printf_d_i(n / 10);
+		if (ret == -1)
+			return (-1);
+		word_len += ret;
+	}
+	ret = ft_putchar((n % 10) + '0');
+	if (ret == -1)
+		return (-1);
 	word_len++;
 	return (word_len);
 }
