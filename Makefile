9115d773d6914e3b06ea1ce557c44738f4f50cae
Kurumi Yamagishi <kuyamagi@student.42tokyo.jp>
2025-04-13 15:16:14 +0900

    かくにん

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: kuyamagi < kuyamagi@student.42tokyo.jp>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/07 15:02:05 by kuyamagi          #+#    #+#              #
-#    Updated: 2025/04/13 14:18:10 by kuyamagi         ###   ########.fr        #
+#    Updated: 2025/04/13 14:53:46 by kuyamagi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libftprintf.a
 
 SRCS    = ft_printf.c ft_c.c ft_s.c ft_d_i.c ft_u.c ft_x.c ft_p.c\
-          utilities/ft_strcpy.c utilities/ft_strlen.c utilities/ft_atoi.c\
+          utilities/ft_strcpy.c utilities/ft_strlen.c
 
 OBJS    = $(SRCS:.c=.o)
 
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kuyamagi < kuyamagi@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 15:10:41 by kuyamagi          #+#    #+#             */
-/*   Updated: 2025/04/13 14:18:17 by kuyamagi         ###   ########.fr       */
+/*   Updated: 2025/04/13 14:57:44 by kuyamagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,6 @@ int		ft_printf_d_i(int n);
 int		ft_printf_u(unsigned int n);
 int		ft_printf_p(void *ptr);
 int		ft_printf_x(unsigned long n, int uppercase);
-char	*ft_pointer(void *ptr);
-char	*ft_itoa(int n);
-size_t	ft_numlen(long nbr);
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 
